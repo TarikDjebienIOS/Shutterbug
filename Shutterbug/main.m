995e56d90f6d3d7e190514b447078df0929caa26
Tarik Djebien <tarik@pc12047.osyris-sa-industrie.local>
2013-05-07 15:33:50 +0200

    Initial Commit

---
+++ Shutterbug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Shutterbug
+//
+//  Created by Tarik Djebien on 07/05/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
